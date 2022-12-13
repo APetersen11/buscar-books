bb883f9653890ce8dd6637f2848c9bab25f28f3b
Alyssa Petersen <aptrsn11@gmail.com>
2022-12-13 17:22:21 -0500

    mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,59 @@
+// saved books login remove user, follow module example
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!){
+        login($email: String!, $password: String!){
+            token
+            user{
+                _id
+                username
+            }
+        }
+    }
+`;
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!){
+        addUser($username: String!, $email: String!, $password: String!){
+            token
+            user{
+                _id
+                username
+            }
+        }
+    }`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($bookData: BookInput!){
+        saveBook($bookData: $bookData){
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                link
+                title
+            }
+        }
+    }`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!){
+        removeBook($bookId: $bookId){
+            _id
+            username
+            email
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                link
+                title
+            } 
+        }
+    }
+`;
